fb4deffc754a39f9538dac1506eba2403d2e32d1
Xavier C Barnes <xcbarnes@gmail.com>
2019-02-24 13:48:47 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -18,6 +18,10 @@ def input_to_index(input)
   return index
 end
 
+def update_array_at_with(array, index, value)
+  array[index] = value
+end
+
 def move(board, input,)
   index = input_to_index(input)
 end
